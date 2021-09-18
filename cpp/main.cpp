ef211882d3ff25657fdc487a862a25bb291f59cb
Saza-ku <the.latticeheart@gmail.com>
2021-09-18 15:00:24 +0900

    main.cpp 直した

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,12 +1,11 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
 #define rep(i, N) for(int i = 0; i < N; i++)
+#define mkp make_pair
 
 using ll = long long;
 const long long INF = 1e18;
 
 int main() {
-  cout << "Hello World!" << endl;
 }
