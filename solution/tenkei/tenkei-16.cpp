895df48bf954db849185779fce229e1ca7bcbf85
Saza-ku <the.latticeheart@gmail.com>
2021-09-18 15:55:29 +0900

    tenkei-16

--- solution/tenkei/tenkei-16.cpp
+++ solution/tenkei/tenkei-16.cpp
@@ -1,16 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int N;
-  cin >> N;
-  set<string> s;
+#define rep(i, N) for (int i = 0; i < N; i++)
+typedef long long ll;
 
-  for (int i = 0; i < N; i++) {
-    string str; cin >> str;
-    if (!s.count(str)) {
-      cout << i + 1 << endl;
-      s.insert(str);
+int main() {
+  ll N;
+  cin >> N;
+  ll a, b, c;
+  cin >> a >> b >> c;
+
+  int res = 100000;
+  rep(i, 10000) {
+    rep(j, i+1) {
+      ll cnt = a * j + (b * (i - j));
+      if (cnt <= N && (N - cnt) % c == 0) {
+        res = min(res, (int)(i + (N - cnt) / c));
+      }
     }
   }
+  cout << res << endl;
 }
