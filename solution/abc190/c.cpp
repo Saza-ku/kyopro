f8fef3c932650b973f37e6c689c03241f2af6544
Saza-ku <the.latticeheart@gmail.com>
2021-11-14 16:41:18 +0900

    abc190-c

--- solution/abc190/c.cpp
+++ solution/abc190/c.cpp
@@ -13,23 +13,40 @@ int main() {
   cin >> N >> M;
   vector<pair<int, int>> constraints(M);
   rep (i, M) {
-    cin >> constraints[i].first >> constraints[i].second;
+    int x, y;
+    cin >> x >> y;
+    constraints[i].first = x;
+    constraints[i].second = y;
   }
 
   int K;
   cin >> K;
   vector<pair<int, int>> c(K);
-  rep (i, K) cin >> c[i].first >> c[i].second;
+  rep (i, K) {
+    int x, y;
+    cin >> x >> y;
+    c[i].first = x;
+    c[i].second = y;
+  }
 
   ll res = 0;
   rep (bit, 1<<K) {
     ll cnt = 0;
-    rep (i, M) {
+    vector<bool> table(N, false);
+    rep (i, K) {
       int x;
       if (bit & 1<<i) x = c[i].first;
       else x = c[i].second;
-
-
+      table[x] = true;
     }
+    rep (i, M) {
+      int x, y;
+      tie(x, y) = constraints[i];
+      if (table[x] && table[y]) cnt++;
+    }
+
+    res = max(res, cnt);
   }
+
+  cout << res << endl;
 }
