a3cf5affe26f4091aed4ee5e1b2bf6526f0d6e69
Saza-ku <the.latticeheart@gmail.com>
2021-11-28 23:05:44 +0900

    abc190-d

--- solution/abc190/d.cpp
+++ solution/abc190/d.cpp
@@ -8,7 +8,28 @@ using ll = long long;
 const int INF = 1e9;
 const ll INFL = 1e18;
 
+ll N;
+
+bool is_ans(ll n) {
+  return (2*N / n - n) % 2 == 1 || (2*N / n - n) % 2 == -1;
+}
+
 int main() {
-  ll N;
   cin >> N;
+
+  ll res = 0;
+  for (ll i = 1; i * i <= 2*N; i++) {
+    if (2*N % i == 0) {
+      ll n = i;
+      if (is_ans(n)) {
+        res++;
+      }
+      if (i * i != N) n = 2*N / i;
+      if (is_ans(n)) {
+        res++;
+      }
+    }
+  }
+
+  cout << res << endl;
 }
