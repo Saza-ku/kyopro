cc455a7580acab5cdb64aa0f9f233b58997498b6
Saza-ku <the.latticeheart@gmail.com>
2022-01-04 00:04:15 +0900

    abc156 早解き

---
+++ solution/abc156/b.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i, N) for(int i = 0; i < N; i++)
+#define mkp make_pair
+
+using ll = long long;
+const int INF = 1e9;
+const ll INFL = 1e18;
+const int MOD = 1e9 + 7;
+
+int main() {
+  ll a, b, N;
+  cin >> N >> a >> b;
+
+  ll res = 0;
+  res += a * (N / (a + b));
+
+  res += min(a, N % (a+b));
+
+  cout << res << endl;
+}
