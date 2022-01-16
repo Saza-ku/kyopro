b45c376102cfce3ccb1c8ff3f1022f40ad1e69c8
Saza-ku <the.latticeheart@gmail.com>
2022-01-17 00:00:17 +0900

    abc116-a

---
+++ solution/abc116/a.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i, N) for(int i = 0; i < N; i++)
+#define mkp make_pair
+
+using ll = long long;
+const int INF = 1e9;
+const ll INFL = 1e18;
+const int MOD = 1e9 + 7;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << a * b / 2 << endl;
+}
