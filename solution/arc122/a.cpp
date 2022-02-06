c0b19b08158641677abeebf56115b2721d1ac888
Saza-ku <the.latticeheart@gmail.com>
2022-02-06 15:13:12 +0900

    スニペットを追加

--- solution/arc122/a.cpp
+++ solution/arc122/a.cpp
@@ -7,19 +7,47 @@ using namespace std;
 using ll = long long;
 const int INF = 1e9;
 const ll INFL = 1e18;
-const int MOD = 1e9 + 7;
-
-ll res = 0;
-
-void dfs() {
-
-}
+const ll MOD = 1e9 + 7;
+const ll MINUS_MOD = MOD - 1;
 
 int main() {
-  int N;
+  ll N;
   cin >> N;
   vector<ll> a(N);
   rep (i, N) cin >> a[i];
 
+  vector<vector<ll>> dp(N, vector<ll>(2));
+  dp[0][0] = 1;
+  dp[0][1] = 0;
 
+  rep (i, N-1) {
+    dp[i+1][0] = (dp[i][0] + dp[i][1]) % MOD;
+    dp[i+1][1] = dp[i][0] % MOD;
+    cerr << dp[i+1][0] << " " << dp[i+1][1] << endl;
+  }
+
+  ll res = 0;
+  for (int i = N - 1; i >= 0; i--) {
+    cerr << dp[i][0] << " " << dp[i][1] << endl;
+    if (i == 0) {
+      res += (dp[i][0] + dp[i][1]) * a[i] % MOD;
+      res %= MOD;
+      break;
+    }
+
+    res += dp[i][0] * a[i];
+    res %= MOD;
+
+    ll minus_a = a[i] * MINUS_MOD;
+    minus_a %= MOD;
+    res += (dp[i][1] * minus_a) % MOD;
+    res %= MOD;
+
+    ll plus_count = dp[i][1] * 2 % MOD;
+    ll minus_count = (dp[i][0] - dp[i][1]) % MOD;
+    dp[i-1][0] = plus_count;
+    dp[i-1][1] = minus_count;
+  }
+  
+  cout << res << endl;
 }
